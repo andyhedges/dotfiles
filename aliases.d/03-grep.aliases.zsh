29acd1419f1f843b25517283d7f4ba043131cc9e
Andy Hedges <andy@hedges.net>
2025-10-19 18:16:27 +0100

    feat: k8s stuff

--- aliases.d/03-grep.aliases.zsh
+++ aliases.d/03-grep.aliases.zsh
@@ -1,4 +1,4 @@
 alias grep='grep -n --color=auto'
 alias grepc='grep -C3 -n --color=auto'
-alias grepf='grep -RIn --color=auto --exclude-dir={.git,node_modules,dist,build}'
+alias grepf='grep -rIn --color=auto --exclude-dir={.git,node_modules,dist,build}'
 
---
+++ functions.d/04-kube.functions.zsh
@@ -0,0 +1,5 @@
+kk() {
+    if [[ $# -eq 0 || "$1" == -* ]]; then kubectl get all -A "$@"; exit; fi
+    names=(); while [[ $# -gt 0 && "$1" != -* ]]; do names+=("$1"); shift; done
+    extra=("$@"); for ns in "${names[@]}"; do echo "===== $ns ====="; kubectl get all -n "$ns" "${extra[@]}"; echo; done
+}
