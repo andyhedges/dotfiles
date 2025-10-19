3e0162db4b187e80b1af4936aa939da9d18f37eb
Andy Hedges <andy@hedges.net>
2025-10-19 18:05:04 +0100

    feat: grep stuff

--- aliases.d/03-grep.aliases.zsh
+++ aliases.d/03-grep.aliases.zsh
@@ -1,2 +1,4 @@
-alias grep='grep --color=auto --exclude-dir={.git,node_modules,dist,build}'
+alias grep='grep --color=auto'
+alias grepc='grep -C3 --color=auto'
+alias grepf='grep -RIn --color=auto --exclude-dir={.git,node_modules,dist,build}'
 
