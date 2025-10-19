06c4bf95e31a46ea6cc8096e24024b40e5055b80
Andy Hedges <andy@hedges.net>
2025-10-19 18:19:18 +0100

    feat: k8s stuff

--- functions.d/04-kube.functions.zsh
+++ functions.d/04-kube.functions.zsh
@@ -15,3 +15,36 @@ kk() {
         echo
     done
 }
+
+kctx() {
+    local contexts ctx_count choice
+    contexts=($(kubectl config get-contexts -o name))
+    ctx_count=${#contexts[@]}
+
+    if (( ctx_count == 0 )); then
+        echo "No kube contexts found."
+        return 1
+    fi
+
+    # If user passed an exact name → just switch
+    if [[ -n "$1" ]]; then
+        kubectl config use-context "$1"
+        return
+    fi
+
+    echo "Available contexts:"
+    local i=1
+    for c in "${contexts[@]}"; do
+        printf "%2d) %s\n" "$i" "$c"
+        ((i++))
+    done
+
+    echo -n "Select context [1-$ctx_count]: "
+    read -r choice
+    if [[ "$choice" =~ ^[0-9]+$ && choice -ge 1 && choice -le $ctx_count ]]; then
+        kubectl config use-context "${contexts[choice-1]}"
+    else
+        echo "Invalid choice."
+        return 1
+    fi
+}
