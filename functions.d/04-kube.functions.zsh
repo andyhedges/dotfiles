11a6b3f37b1a394a6078aecc09327e322cca0161
Andy Hedges <andy@hedges.net>
2025-10-19 18:23:28 +0100

    feat: k8s stuff

--- functions.d/04-kube.functions.zsh
+++ functions.d/04-kube.functions.zsh
@@ -3,6 +3,24 @@ kk() {
         kubectl get all -A "$@"
         return
     fi
+    names=()
+    while [[ $# -gt 0 && "$1" != -* ]]; do
+        names+=("$1")
+        shift
+    done
+    extra=("$@")
+    for ns in "${names[@]}"; do
+        echo "===== $ns ====="
+        kubectl get all -n "$ns" "${extra[@]}"
+        echo
+    done
+}
+
+kk2() {
+    if [[ $# -eq 0 || "$1" == -* ]]; then
+        kubectl get all -A "$@"
+        return
+    fi
 
     names=()
     while [[ $# -gt 0 && "$1" != -* ]]; do
