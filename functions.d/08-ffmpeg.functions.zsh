9a86d975bb7d916968d02809352fdf6a61505ef2
Andy Hedges <andy@hedges.net>
2025-11-05 20:37:33 +0000

    feat: ffmpeg stuff

---
+++ functions.d/08-ffmpeg.functions.zsh
@@ -0,0 +1,16 @@
+
+# ffhevc50: Transcode video to HEVC using VideoToolbox at 50 Mbps
+ffhevc50() {
+  if [ $# -eq 0 ]; then
+    echo "Usage: ffhevc50 <inputfile>"
+    return 1
+  fi
+
+  local infile="$1"
+  local outfile="${infile%.*}_processed.${infile##*.}"
+
+  ffmpeg -i "$infile" \
+    -c:v hevc_videotoolbox -b:v 50M -maxrate 50M -bufsize 100M -tag:v hvc1 \
+    -pix_fmt yuv420p -c:a copy -movflags +faststart \
+    "$outfile"
+}
