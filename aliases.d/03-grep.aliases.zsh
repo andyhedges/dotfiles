f032ff118dfdd018de528de7fed15bf92395eb68
Andy Hedges <andy@hedges.net>
2025-12-21 20:41:45 +0000

    feat: made grep not give additional output when piped

--- aliases.d/03-grep.aliases.zsh
+++
@@ -1,4 +0,0 @@
-alias grep='grep -n --color=auto'
-alias grepc='grep -C3 -n --color=auto'
-alias grepf='grep -rIn --color=auto --exclude-dir={.git,node_modules,dist,build}'
-
---
+++ functions.d/03-grep.functions.zsh
@@ -0,0 +1,25 @@
+grep() {
+  if [[ -t 1 ]]; then
+    command grep -n --color=auto "$@"
+  else
+    command grep "$@"
+  fi
+}
+
+grepc() {
+  if [[ -t 1 ]]; then
+    command grep -C3 -n --color=auto "$@"
+  else
+    command grep -C3 "$@"
+  fi
+}
+
+grepf() {
+  if [[ -t 1 ]]; then
+    command grep -rIn --color=auto \
+      --exclude-dir={.git,node_modules,dist,build} "$@"
+  else
+    command grep -rI \
+      --exclude-dir={.git,node_modules,dist,build} "$@"
+  fi
+}
