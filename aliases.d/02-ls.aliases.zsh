fee67cf7790177b3f7fe9f03ce09730034b55050
Andy Hedges <andy@hedges.net>
2026-01-19 21:06:01 +0000

    fix: now tab complete should work for ls

--- aliases.d/02-ls.aliases.zsh
+++ aliases.d/02-ls.aliases.zsh
@@ -2,10 +2,13 @@
 
 if command -v eza >/dev/null 2>&1; then
   alias ls='eza --group-directories-first --icons'
+  compdef eza=ls
 elif command -v lsd >/dev/null 2>&1; then
   alias ls='lsd --group-dirs first'
+  compdef lsd=ls
 elif command -v exa >/dev/null 2>&1; then
   alias ls='exa --group-directories-first --icons'
+  compdef exa=ls
 fi
 alias ll='ls -lh'
 alias la='ls -lha'
